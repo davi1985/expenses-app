f71d22391162411b10679c33b1c6c342d9c1292b
Davi Silva <davi.psilva@midway.com.br>
2023-10-05 17:49:31 -0300

    list expenses

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,5 +1,15 @@
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 
-export const ExpensesList = () => {
-  return <FlatList />;
+const renderExpenseItem = (itemData) => {
+  return <Text>{itemData.item.description}</Text>;
+};
+
+export const ExpensesList = ({ expenses }) => {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={({ id }) => id}
+    />
+  );
 };
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -43,7 +43,7 @@ export const ExpensesOutput = ({ expenses, expensesPeriodName }) => {
         periodName={expensesPeriodName}
       />
 
-      <ExpensesList />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 };
