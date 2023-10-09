359650d6bfe9a22669aee6b1216d8d20acdb163e
Davi Silva <davi.psilva@midway.com.br>
2023-10-09 20:41:41 -0300

    adding validation

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -10,26 +10,38 @@ export const ExpenseForm = ({
   onSubmit,
   defaultValues,
 }) => {
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : "",
-    date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : "",
-    description: defaultValues ? defaultValues.description.trim() : "",
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : "",
+      isValid: true,
+    },
+    date: {
+      value: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : "",
+      isValid: true,
+    },
+    description: {
+      value: defaultValues ? defaultValues.description.trim() : "",
+      isValid: true,
+    },
   });
 
   const inputChangeHandler = (inputIdentifier, enteredValue) => {
-    setInputValues((currentInputValues) => {
+    setInputs((currentInputs) => {
       return {
-        ...currentInputValues,
-        [inputIdentifier]: enteredValue,
+        ...currentInputs,
+        [inputIdentifier]: {
+          value: enteredValue,
+          isValid: true,
+        },
       };
     });
   };
 
   const submitHandler = () => {
     const expenseData = {
-      amount: +inputValues.amount,
-      date: new Date(inputValues.data),
-      description: inputValues.description,
+      amount: +inputs.amount.value,
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
 
     const amountValid = isNaN(expenseData.amount) && expenseData.amount > 0;
@@ -37,12 +49,25 @@ export const ExpenseForm = ({
     const descriptionIsValid = expenseData.description.trim().length > 0;
 
     if (!amountValid || !dateIsValid || !descriptionIsValid) {
-      Alert.alert("Invalid input", "Please check your input values");
+      setInputs((currentInput) => ({
+        amount: { value: currentInput.amount.value, isValid: amountValid },
+        date: { value: currentInput.date.value, isValid: dateIsValid },
+        description: {
+          value: currentInput.description.value,
+          isValid: descriptionIsValid,
+        },
+      }));
+
       return;
     }
 
     onSubmit(expenseData);
   };
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
 
   return (
     <View style={styles.form}>
@@ -55,7 +80,7 @@ export const ExpenseForm = ({
           textInputConfig={{
             keyboardType: "decimal-pad",
             onChangeText: inputChangeHandler.bind(this, "amount"),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
 
@@ -66,7 +91,7 @@ export const ExpenseForm = ({
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
             onChangeText: inputChangeHandler.bind(this, "date"),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
@@ -76,9 +101,13 @@ export const ExpenseForm = ({
         textInputConfig={{
           multiline: true,
           onChangeText: inputChangeHandler.bind(this, "description"),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+
+      {formIsInvalid && (
+        <Text>Invalid input values - please check your entered data!</Text>
+      )}
 
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={onCancel}>
